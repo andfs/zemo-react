06a2344dcce423d43b80a8f08118cdfce62aa3c6
Anderson de Faria Silva <andfariasilva@gmail.com>
2016-07-23 22:01:47 -0300

    segmentedControl

--- src/comp/segmentedControl.js
+++ src/comp/segmentedControl.js
@@ -53,22 +53,38 @@ class SegmentedControlAndroid extends Component {
 
 class SegmentedControlIphone extends Component {
 
-	constructor(props) {
-	  super(props);
-	
-	  this.state = {
-	  	selectedIndex: 0
-	  };
-	}
-
 	onChange(event) {
 		let index = event.nativeEvent.selectedSegmentIndex;
+		switch(index)
+		{
+			case 0:
+				this.props.home.setState({procurarVaga: true, abaSelecionada: 0});
+				break;
+
+			case 1:
+				this.props.home.setState({procurarVaga: false, abaSelecionada: 1});
+				this.props.home.estacionar();
+				break;
+
+			case 2:
+				this.props.home.setState({procurarVaga: true, abaSelecionada: 0});
+				this.props.home.liberarVaga();
+				break;
+		}
 	}
 	
 	render() {
+		let options = [];
+		if(this.props.abaSelecionada == 1) {
+			options = [" Procurando Vagas ", "    Estacionado    ", "   Liberar Vaga   "];
+		}
+		else {
+			options = [" Procurando Vagas ", "    Estacionar    ", "   Liberar Vaga   "];	
+		}
+
 		return (
 				<SegmentedControlIOS 
-				  values={["Vagas", "Estacionar", "LiberarVaga"]}
+				  values={options}
 				  selectedOption={this.state.selectedIndex}
 				  onChange={this.onChange.bind(this)}
 				/>
