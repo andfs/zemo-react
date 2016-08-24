a5f493409f980a0ce50c70a81a4eaf9078309b0a
Anderson de Faria Silva <andfariasilva@gmail.com>
2016-08-24 15:49:23 -0300

    layout nova reserva

--- src/views/novaReserva.js
+++ src/views/novaReserva.js
@@ -16,8 +16,59 @@ import Moment from 'moment';
 import convertePlaca from '../functions/funcoesPlacas';
 import ParkoButton from '../comp/parkoButton';
 import ParkoTitulo from '../comp/parkoTitulo';
+import { stylesGeral, color } from '../estilos/geral';
 
+class NovaReservaComponente extends Component {
 
+	render() {
+		return(
+			<View>
+				<View style={styles.linha}>
+					<Text style={stylesGeral.topico}>Previsão de chegada:</Text>
+					<DatePicker
+			          style={{marginTop: 5, width: 210}}
+			          date={this.props.pai.state.dtChegadaPrevista}
+			          mode="datetime"
+			          format="DD/MM/YYYY HH:mm"
+			          confirmBtnText="OK"
+			          cancelBtnText="Cancelar"
+			          showIcon={true}
+			          customStyles={{
+					    dateText: {
+					      color: 'white'
+					    },
+					    dateInput: {
+					    	borderColor: color.dark3
+					    }
+					  }}
+			          onDateChange={(datetime) => {this.props.pai.setState({dtChegadaPrevista: datetime})}}
+			        />
+			    </View>
+
+				<View style={styles.linha}>
+					<Text style={stylesGeral.topico}>Tipo de permanência:</Text>
+					<View style={{marginTop: 5, borderWidth: 1,borderColor: color.dark3, width: 200}}>
+						<Picker style={styles.picker} selectedValue={this.props.pai.state.tipoPermanencia}
+						  onValueChange={(tipoPermanencia) => this.props.pai.setState({tipoPermanencia: tipoPermanencia})}>
+						  <Picker.Item label="Hora" value="horista" />
+						  <Picker.Item label="Diária" value="diarista" />
+						  <Picker.Item label="Pernoite" value="pernoite" />
+						  <Picker.Item label="Mensalista" value="mensalista" />
+						</Picker>
+					</View>
+				</View>
+
+				<View style={styles.botoes}>
+					<ParkoButton onPress={this.props.reservar} texto="Reservar" tamanho="medio"/>
+					<ParkoButton onPress={this.props.voltar} texto="Voltar" tamanho="medio"/>
+				</View>
+				<Text>
+		            {this.props.erro}
+		        </Text>
+			</View>
+		);
+	}
+}
 export default class NovaReserva extends Component {
 
 	constructor(props) {
@@ -25,7 +76,6 @@ export default class NovaReserva extends Component {
 	    this.state = {
 	    	placa: '',
 	    	dtChegadaPrevista: Moment().add(2, 'hour').toDate(),
-	    	data: Moment().add(2, 'hour').toDate(),
 	    	tipoPermanencia: '',
 	    	estacionamentoId: '',
 	    	userId: '',
@@ -34,46 +84,49 @@ export default class NovaReserva extends Component {
 	}
 
 	reservar() {
-		let placa = convertePlaca(this.state.placa);
-		if(placa === "erro") {
-			Alert.alert(
-			  "Ooops...",
-			  "Formato inválido da placa. Deve ser informado no seguinte formato: XXX-9999",
-			  [
-			    {text: 'OK', onPress: () => console.log('OK Pressed')},
-			  ]
-			);
-		}
-		else {
-			let context = this;
-			Meteor.collection('reservas').insert({
-												  placa: placa, 
-												  dtReserva: new Date(), 
-												  dtChegadaPrevista: this.state.dtChegadaPrevista,
-												  tipoPermanencia: this.state.tipoPermanencia,
-												  estacionamentoId: this.props.estacionamento._id,
-												  userId: Meteor.userId() }, function(error) {
+		const { placas, ready } = this.props;
+		if(ready) {
+			let placa = convertePlaca(this.state.placa.length > 0 ? this.state.placa : placas[0].placa);
+			if(placa === "erro") {
+				Alert.alert(
+				  "Ooops...",
+				  "Formato inválido da placa. Deve ser informado no seguinte formato: XXX-9999. Placa: " + this.state.placa,
+				  [
+				    {text: 'OK', onPress: () => console.log('OK Pressed')},
+				  ]
+				);
+			}
+			else {
+				let context = this;
+				Meteor.collection('reservas').insert({
+													  placa: placa, 
+													  dtReserva: new Date(), 
+													  dtChegadaPrevista: this.state.dtChegadaPrevista,
+													  tipoPermanencia: this.state.tipoPermanencia,
+													  estacionamentoId: this.props.estacionamento._id,
+													  userId: Meteor.userId() }, function(error) {
 
 
-				if(error) {
-					Alert.alert(
-					  "Ooops...",
-					  error.message,
-					  [
-					    {text: 'OK', onPress: () => console.log('OK Pressed')},
-					  ]
-					);
-				}
-				else {
-					Alert.alert(
-					  "Sucesso",
-					  "Sua reserva está confirmada. Você tem até 15 minutos de tolerância para manter sua reserva.",
-					  [
-					    {text: 'OK', onPress: () => context.props.navigator.pop()},
-					  ]
-					);
-				}
-			});
+					if(error) {
+						Alert.alert(
+						  "Ooops...",
+						  error.message,
+						  [
+						    {text: 'OK', onPress: () => console.log('OK Pressed')},
+						  ]
+						);
+					}
+					else {
+						Alert.alert(
+						  "Sucesso",
+						  "Sua reserva está confirmada. Você tem até 15 minutos de tolerância para manter sua reserva.",
+						  [
+						    {text: 'OK', onPress: () => context.props.navigator.pop()},
+						  ]
+						);
+					}
+				});
+			}
 		}
 	}
 
@@ -91,43 +144,13 @@ export default class NovaReserva extends Component {
 				return(
 					<View style={styles.container}>
 						<ParkoTitulo texto="Nova Reserva"/>
-						<View style={styles.linha}>
-							<Text style={styles.texto}>Placa</Text>
-							<TextInput value={placas.placa} editable={false}/>
+						<View style={styles.umaPlaca}>
+							<Text style={stylesGeral.topico}>Placa: </Text>
+							<Text style={stylesGeral.respostaTopico}> {convertePlaca(placas[0].placa)} </Text>
 						</View>
 
-						<View style={styles.linha}>
-							<Text style={styles.texto}>Previsão de chegada</Text>
-							<DatePicker
-					          style={{width: 200}}
-					          date={this.state.data}
-					          mode="datetime"
-					          format="DD/MM/YYYY HH:mm"
-					          confirmBtnText="OK"
-					          cancelBtnText="Cancelar"
-					          showIcon={true}
-					          onDateChange={(datetime) => {this.setState({dtChegadaPrevista: datetime});}}
-					        />
-					    </View>
-
-						<View style={styles.linha}>
-							<Text style={styles.texto}>Tipo de permanência</Text>
-							<Picker
-							  onValueChange={(tipoPermanencia) => this.setState({tipoPermanencia: tipoPermanencia})}>
-							  <Picker.Item label="Horista" value="horista" />
-							  <Picker.Item label="Diarista" value="diarista" />
-							  <Picker.Item label="Pernoite" value="pernoite" />
-							  <Picker.Item label="Mensalista" value="mensalista" />
-							</Picker>
-						</View>
-
-						<View style={styles.botoes}>
-							<ParkoButton onPress={this.reservar.bind(this)} texto="Reservar" tamanho="pequeno"/>
-							<ParkoButton onPress={this.voltar.bind(this)} texto="Voltar" tamanho="pequeno"/>
-						</View>
-						<Text>
-				            {this.state.erro}
-				        </Text>
+						<NovaReservaComponente erro={this.state.erro} pai={this} data={this.state.dtChegadaPrevista}
+							reservar={this.reservar.bind(this)} voltar={this.voltar.bind(this)}/>
 					</View>
 				);
 			}
@@ -136,7 +159,7 @@ export default class NovaReserva extends Component {
 					<View style={styles.container}>
 						<ParkoTitulo texto="Nova Reserva"/>
 						<View style={styles.linha}>
-							<Text style={styles.texto}>Placa</Text>
+							<Text style={stylesGeral.topico}>Placa</Text>
 							<Picker onValueChange={(placa) => this.setState({placa: placa})} selectedValue={this.state.placa}>
 								{placas.map((placa)=> (
 									<Picker.Item label={placa.placa} value={placa.placa} key={placa.placa}/>
@@ -144,37 +167,8 @@ export default class NovaReserva extends Component {
 							</Picker>
 						</View>
 
-						<View style={styles.linha}>
-							<Text style={styles.texto}>Previsão de chegada</Text>
-							<DatePicker
-					          style={{width: 200}}
-					          date={this.state.data}
-					          mode="datetime"
-					          format="DD/MM/YYYY HH:mm"
-					          confirmBtnText="OK"
-					          cancelBtnText="Cancelar"
-					          showIcon={true}
-					          onDateChange={(datetime) => {this.setState({dtChegadaPrevista: datetime, data: datetime})}}
-					        />
-					    </View>
-
-						<View style={styles.linha}>
-							<Text style={styles.texto}>Tipo de permanência</Text>
-							<Picker onValueChange={(tipoPermanencia) => this.setState({tipoPermanencia: tipoPermanencia})}>
-							  <Picker.Item label="Horista" value="horista" />
-							  <Picker.Item label="Diarista" value="diarista" />
-							  <Picker.Item label="Pernoite" value="pernoite" />
-							  <Picker.Item label="Mensalista" value="mensalista" />
-							</Picker>
-						</View>
-
-						<View style={styles.botoes}>
-							<ParkoButton onPress={this.reservar.bind(this)} texto="Reservar" tamanho="pequeno"/>
-							<ParkoButton onPress={this.voltar.bind(this)} texto="Voltar" tamanho="pequeno"/>
-						</View>
-						<Text>
-				            {this.state.erro}
-				        </Text>
+						<NovaReservaComponente erro={this.state.erro} pai={this} data={this.state.dtChegadaPrevista}
+							reservar={this.reservar.bind(this)} voltar={this.voltar.bind(this)}/>
 					</View>
 				);
 			}
@@ -183,42 +177,12 @@ export default class NovaReserva extends Component {
 					<View style={styles.container}>
 						<ParkoTitulo texto="Nova Reserva"/>
 						<View style={styles.linha}>
-							<Text style={styles.texto}>Placa</Text>
-							<TextInput value={placas.placa} onChangeText={(val) => this.setState({placa: val})}/>
+							<Text style={stylesGeral.topico}>Placa</Text>
+							<TextInput onChangeText={(val) => this.setState({placa: val})} style={stylesGeral.respostaTopico}/>
 						</View>
 
-						<View style={styles.linha}>
-							<Text style={styles.texto}>Previsão de chegada</Text>
-							<DatePicker
-					          style={{width: 200}}
-					          date={Moment().toDate()}
-					          mode="datetime"
-					          format="DD/MM/YYYY HH:mm"
-					          confirmBtnText="OK"
-					          cancelBtnText="Cancelar"
-					          showIcon={true}
-					          onDateChange={(datetime) => {this.setState({dtChegadaPrevista: datetime, data: datetime})}}
-					        />
-					    </View>
-
-						<View style={styles.linha}>
-							<Text style={styles.texto}>Tipo de permanência</Text>
-							<Picker
-							  onValueChange={(tipoPermanencia) => this.setState({tipoPermanencia: tipoPermanencia})}>
-							  <Picker.Item label="Horista" value="horista" />
-							  <Picker.Item label="Diarista" value="diarista" />
-							  <Picker.Item label="Pernoite" value="pernoite" />
-							  <Picker.Item label="Mensalista" value="mensalista" />
-							</Picker>
-						</View>
-
-						<View style={styles.botoes}>
-							<ParkoButton onPress={this.reservar.bind(this)} texto="Reservar" tamanho="pequeno"/>
-							<ParkoButton onPress={this.voltar.bind(this)} texto="Voltar" tamanho="pequeno"/>
-						</View>
-						<Text>
-				            {this.state.erro}
-				        </Text>
+						<NovaReservaComponente erro={this.state.erro} pai={this} data={this.state.dtChegadaPrevista}
+							reservar={this.reservar.bind(this)} voltar={this.voltar.bind(this)}/>
 					</View>
 				);
 			}
@@ -240,27 +204,21 @@ const styles = StyleSheet.create({
   	flex: 1,
   	marginLeft: 10
   },
+  umaPlaca: {
+  	flexDirection: 'row',
+  },
   linha: {
   	marginTop: 20
   },
-  texto: {
-  	marginLeft: 6
-  },
   botoes: {
-  	flexDirection: 'row',
-  	flex: 1,
-  	marginTop: 20
+  	marginTop: 40,
+  	alignItems: 'center',
+  	justifyContent: 'center'
   },
-  button: {
-    height: 36,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    flex: 1
+  picker: {
+   color: color.light2,
+   borderColor: color.dark3,
+   height: 40,
   }
   
 });
