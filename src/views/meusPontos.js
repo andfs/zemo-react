7d1140a4e15a3ccf04f2d9ea7635e15e5d978ea5
Anderson de Faria Silva <andfariasilva@gmail.com>
2016-08-25 12:02:54 -0300

    layout meus pontos

--- src/views/meusPontos.js
+++ src/views/meusPontos.js
@@ -11,6 +11,7 @@ import Moment from 'moment';
 import convertePlaca from '../functions/funcoesPlacas';
 import ParkoButton from '../comp/parkoButton';
 import ParkoTitulo from '../comp/parkoTitulo';
+import { stylesGeral, color } from '../estilos/geral';
 
 export default class MeusPontos extends Component {
 
@@ -30,11 +31,16 @@ export default class MeusPontos extends Component {
 			return(
 				<View style={styles.container}>
 					<ParkoTitulo texto="Meus Pontos"/>
-					<View style={{alignItems: 'center', justifyContent: 'center'}}>
-						<Text style={styles.pontos}>{pontos}</Text>
-						<Text style={styles.descricao}>Troque seus ponts por horas grátis em estacionamentos.</Text>
+					<View style={{alignItems: 'center', justifyContent: 'center', marginTop: 60}}>
+						<View style={{flexDirection: 'row', marginBottom: 15}}>
+							<Text style={styles.pontos}>{pontos}</Text>
+							<View style={{justifyContent: 'flex-end'}}>
+								<Text style={styles.pontosDescricao}>pts</Text>
+							</View>
+						</View>
+						<Text style={stylesGeral.topico}>Troque seus pontos por horas em estacionamentos.</Text>
 					</View>
-					<View style={{alignItems: 'stretch', justifyContent: 'center'}}>
+					<View style={{alignItems: 'stretch', justifyContent: 'center', marginTop: 10}}>
 						<ParkoButton onPress={this.onPress.bind(this)} texto="Trocar pontos"/>
 					</View>
 				</View>
@@ -56,5 +62,16 @@ const styles = StyleSheet.create({
   container: {
   	alignItems: 'center',
   	justifyContent: 'center'
+  },
+  pontos: {
+  	color: color.light1,
+  	fontSize: 42,
+  	fontWeight: 'bold'
+  },
+  pontosDescricao: {
+  	color: color.light1,
+  	fontSize: 11,
+  	marginLeft: 5,
+  	paddingBottom: 9
   }
 });
